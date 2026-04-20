2f54945a2b21bc18bb0d9a5555ffa4ca9a33682c
Manjunatha Bhajantri <manjubhajantri316@gmail.com>
2026-04-20 22:14:50 +0530

    Add files via upload

---
+++ dataset/GOLD.dim_product.sql
@@ -0,0 +1,20 @@
+if OBJECT_ID('gold.dim_product','V') IS NOT NULL
+  drop view gold.dim_product 
+GO
+CREATE VIEW gold.dim_product as
+SELECT
+ROW_NUMBER() OVER(ORDER BY pr.prd_start_dt,pr.prd_key) as product_key,
+pr.prd_id as product_id,
+pr.prd_key as product_number,
+pr.prd_nm as product_name,
+pr.cat_id as category_id,
+px.CAT as category ,
+px.SUBCAT as sub_category,
+px.MAINTENANCE as maintenance,
+pr.prd_cost as product_cost,
+pr.prd_line as product_line,
+pr.prd_start_dt as start_date
+FROM SILVER.crm_prd_info pr
+LEFT JOIN SILVER.ERP_PX_CAT_G1V2 px
+on pr.cat_id=px.ID
+where pr.prd_end_dt IS NULL
